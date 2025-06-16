9b5b7e979638d397ae1855461f347c791b959853
Matheus <matheus.bandeira74@gmail.com>
2025-06-16 20:31:35 -0300

    analise exploratória univariada

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import seaborn as sns 
 from IPython.display import display
+import matplotlib.pyplot as plt
+from matplotlib.colors import rgb2hex
+from matplotlib import cm
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
 
+#df=pd.read_csv('data/processed/bank_marketing_processed.csv')
 
 def analise_univariada(data, variavel):
     """
@@ -21,25 +28,57 @@ def analise_univariada(data, variavel):
 
     # Tabela de frequência 
     tabela_frequencia = data[variavel].value_counts().reset_index()
-    tabela_frequencia.columns = [variavel, 'Frequencia']
-    tabela_frequencia['Percent'] = (tabela_frequencia['Frequencia'] / len(data) * 100).round(2)
+    tabela_frequencia.columns = [variavel, 'Frequency']
+    tabela_frequencia['Percent'] = (tabela_frequencia['Frequency'] / len(data) * 100).round(2)
     # linha total
     df_total = pd.DataFrame({
             variavel: 'Total',
-            'Frequencia': tabela_frequencia['Frequencia'].sum(),
+            'Frequency': tabela_frequencia['Frequency'].sum(),
             'Percent': [100.0]
 
         })
 
-    tabela_frequencia = pd.concat([tabela_frequencia, df_total], ignore_index=True)
+    tabela_total = pd.concat([tabela_frequencia, df_total], ignore_index=True)
 
     print(f'Tabela de Frequencia {variavel}')
-    display(tabela_frequencia)
+    display(tabela_total)
 
-    # Gráfico com a frequência
-    sns.barplot(x=variavel, y='Frequencia', data=tabela_frequencia[:-1])
+    # Aplica palleta de cores contínua
+    valores = tabela_frequencia['Frequency'].values
+    norm = (valores - valores.min())/(valores.max()- valores.min())
 
+    cmap = cm.get_cmap('Blues')  # Use a colormap with 256 colors
+    cores = [cmap(n) for n in norm]
+    cores_hex = [rgb2hex(c[:3]) for c in cores]
 
+    plt.figure(figsize=(10,5))
+    ax = sns.barplot(x=variavel, y='Frequency', data=tabela_frequencia, palette=cores_hex)
+    for spine in ['top', 'right', 'left', 'bottom']:
+        ax.spines[spine].set_visible(False)
+    for p in ax.patches:
+        altura = p.get_height()
+        cor_barra = p.get_facecolor()
+  
+        luminancia = 0.299 * cor_barra[0] + 0.587 * cor_barra[1] + 0.114 * cor_barra[2]
+        cor_texto = 'black' if luminancia > 0.6 else 'white'
+    
+        ax.text(
+            x=p.get_x() + p.get_width() / 2,
+            y=altura / 2,
+            s=f'{int(altura)}',
+            ha='center',
+            va='center',
+            color=cor_texto,
+            fontweight='bold',
+            fontsize=8
+        )
+    plt.title(f'Frequency of {variavel}', fontsize=12, fontweight='bold')
+    plt.xticks(rotation=45)
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    plt.show()
+
+#analise_univariada(df, 'education')
 
 
 
