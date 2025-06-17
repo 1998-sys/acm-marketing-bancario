f89e9755223bee34abbc86263269abbfd34b9119
Matheus <matheus.bandeira74@gmail.com>
2025-06-17 20:53:11 -0300

    chi2 + tab contigenci

--- src/acm_analysis.py
+++ src/acm_analysis.py
@@ -9,7 +9,7 @@ from IPython.display import display
 
 
 
-def tab_contingencia(df):
+def chi2(df):
     """
     Cria e imprime todas as tabelas de contingência possíveis entre as variáveis categóricas do DataFrame
     :param df: DataFrame contendo os dados
@@ -20,11 +20,11 @@ def tab_contingencia(df):
         print(f'Tabela de Contingência entre {item[0]} e {item[1]}')
         tabela_contingencia = pd.crosstab(df[item[0]], df[item[1]])
 
-        #display(tabela_contingencia)
+        display(tabela_contingencia)
 
         chi2, pvalor, gl, freq_esp = chi2_contingency(tabela_contingencia)
         print(f'Estatística qui-quadrado: {chi2}')
-        print(f'p-valor da estatística: {round(pvalor,4)}','\n')
-
+        print(f'p-valor da estatística: {round(pvalor,4)}')
+        print(f'Graus de liberdade: {gl}', '\n')
 
 
